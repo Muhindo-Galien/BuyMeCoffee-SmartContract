7437630183b442ae6ec0c64c2e5835d7f64c1d82
Muhindo-Galien <muhindogalien512@gmail.com>
2022-11-01 23:17:32 +0300

    deploy stripts added!

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,34 @@
-require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+
+const GOERLI_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: {
-    compilers: [
-        {
-            version: "0.8.7",
-        }
-    ],
+  networks: {
+    hardhat: {
+        chainId: 31337,
+    },
+    localhost: {
+        chainId: 31337,
+    },
+    goerli: {
+        url: GOERLI_URL,
+        accounts: [PRIVATE_KEY],
+        chainId: 5,
+        blockConfirmations: 6,
+    },
 },
+  solidity: "0.8.7"
 };
